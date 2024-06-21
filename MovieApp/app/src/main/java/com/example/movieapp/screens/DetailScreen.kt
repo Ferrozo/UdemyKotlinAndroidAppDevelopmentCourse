2319602fe1f383b05248a0daa9cd942765579ce4
ferrozo <ferrozo.c@gmail.com>
2024-06-21 20:23:07 +0100

    Home done

--- MovieApp/app/src/main/java/com/example/movieapp/screens/DetailScreen.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/screens/DetailScreen.kt
@@ -1,5 +1,6 @@
 package com.example.movieapp.screens
 
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
@@ -13,10 +14,14 @@ fun DetailScreen(
     movieData: String?
 ){
     Surface(
-        modifier = Modifier.fillMaxSize(),
+        modifier =
+        Modifier.fillMaxSize(),
         color = DarkColor
     )
     {
+        Column {
+
+        }
 
     }
 }
