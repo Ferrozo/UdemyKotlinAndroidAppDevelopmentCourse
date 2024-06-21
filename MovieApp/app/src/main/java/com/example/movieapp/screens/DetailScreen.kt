87efd9a03c9dec3aa71826db409ff5a03267e5c3
ferrozo <ferrozo.c@gmail.com>
2024-06-21 20:22:10 +0100

    Images from network

--- MovieApp/app/src/main/java/com/example/movieapp/screens/DetailScreen.kt
+++ MovieApp/app/src/main/java/com/example/movieapp/screens/DetailScreen.kt
@@ -1,9 +1,22 @@
 package com.example.movieapp.screens
 
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.navigation.NavController
+import com.example.movieapp.ui.theme.DarkColor
 
 @Composable
-fun DetailScreen(navController: NavController, movieData: String?){
+fun DetailScreen(
+    navController: NavController,
+    movieData: String?
+){
+    Surface(
+        modifier = Modifier.fillMaxSize(),
+        color = DarkColor
+    )
+    {
 
+    }
 }
